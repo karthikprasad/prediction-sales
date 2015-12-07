a719961715675bb88d8e81270996919d1376ffc2
Karthik Prasad <prasadkr@uci.edu>
2015-12-07 01:00:40 -0800

    outlier removal

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -7,15 +7,70 @@ from sklearn.ensemble import RandomForestRegressor
 
 DATA_DIR = '../data/'
 
+def read_data():
+    print('Extract the Training, Test, Store and States csv file')
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 170)
+    types = {   'Store': np.dtype(str),
+                #'DayOfWeek': np.dtype(int),
+                #'Open': np.dtype(str),
+                #'Promo': np.dtype(str),
+                'CompetitionDistance' : np.dtype(float),
+                'CompetitionOpenSinceYear': np.dtype(float),
+                'CompetitionOpenSinceMonth': np.dtype(float),
+                #'StateHoliday': np.dtype(str),
+                'Promo2SinceWeek': np.dtype(float),
+                'Promo2SinceYear': np.dtype(float)}
+                #'SchoolHoliday': np.dtype(str),
+                #'PromoInterval': np.dtype(str)}
+
+    train = pd.read_csv(DATA_DIR+'train.csv', parse_dates=[2], dtype=types)
+    test = pd.read_csv(DATA_DIR+'test.csv', parse_dates=[3], dtype=types)
+    store = pd.read_csv(DATA_DIR+'store.csv', dtype=types)
+    store_states = pd.read_csv(DATA_DIR+'store_states.csv', dtype=types)
+
+    print 'Merging Training and Test CSV with Store and State CSV'
+    train = pd.merge(train, store, on='Store')
+    train = pd.merge(train, store_states, on='Store')
+    test = pd.merge(test, store, on='Store')
+    test = pd.merge(test, store_states, on='Store')
+
+    columns = train.columns.tolist()
+    del(columns[columns.index('Sales')])
+    columns.append('Sales')
+    train = train[columns]
+
+    return train, test
+
+
 # Feature Engineering
-def build_features(data):
+def engineer_data(data):
+    features = []
+    # --> Remove Store as we don't want the individual store to affect the prediction.
+    #     Instead, look at the other charecteristics
+    # Add Date
+    features.append('Date')
+    # Convert DayOfWeek to dummy variables
+    for x in range(0,7):
+        features.append('DayOfWeek' + str(x+1))
+        data['DayOfWeek' + str(x+1)] = data.Date.dt.dayofweek.map(lambda y: 1 if y == x else 0)
+
+    # Add a special feature to capture the store opening on Sunday
+    features.append('SundayOpen')
+    data.loc[(data.DayOfWeek7 == 1) & (data.Open == 1), 'SundayOpen'] = 1
+    data.loc[data.SundayOpen.isnull(), 'SundayOpen'] = 0
+    
+    data.loc[data.Store == str(x), 'Sales'] = data[data.Store == str(x)].Sales.clip(data[data.Store == str(x)].Sales.mean() - 3*data[data.Store == str(x)].Sales.std(), data[data.Store == str(x)].Sales.mean() + 3*data[data.Store == str(x)].Sales.std())
+    
+    
+
+    features.extend(['Date', 'Store', 'CompetitionDistance', 'Promo', 'Promo2', 'SchoolHoliday', 'StoreType', 'Assortment',\
+                     'StateHoliday', 'State'])
     # Handle missing Data
     data.loc[data.Open.isnull(), 'Open'] = (data['DayOfWeek'] != 7).astype(int)
 
     # Use following features directly
-    features = []
-    features.extend(['Date', 'Store', 'CompetitionDistance', 'Promo', 'Promo2', 'SchoolHoliday', 'StoreType', 'Assortment',\
-                     'StateHoliday', 'State'])
+    
 
     # Mapping data of following features to numbers
     mappings = {'0':0, 'a':1, 'b':2, 'c':3, 'd':4}
@@ -75,44 +130,17 @@ def build_features(data):
 def rmspe(exp, pred):
     return np.sqrt(np.mean(((exp - pred)/exp) ** 2))
 
+def remove_outliers(data):
+    data = data[data.Sales > 0]
+    # Clip Sales for all stores to remove outliers
+    for store_id in range(1,1136):
+        data_store = data[data.Store == str(store_id)]
+        mean_sales = data_store.Sales.mean()
+        std_sales = data_store.Sales.std()
+        data.loc[data.Store == str(store_id), 'Sales'] = data_store.Sales.clip(mean-3.3*std_sales, mean+3.3*std_sales)
 
-def combine_data():
-    print('Extract the Training, Test, Store and States csv file')
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 170)
-    types = {   'Store': np.dtype(str),
-                #'DayOfWeek': np.dtype(int),
-                #'Open': np.dtype(str),
-                #'Promo': np.dtype(str),
-                'CompetitionDistance' : np.dtype(float),
-                'CompetitionOpenSinceYear': np.dtype(float),
-                'CompetitionOpenSinceMonth': np.dtype(float),
-                #'StateHoliday': np.dtype(str),
-                'Promo2SinceWeek': np.dtype(float),
-                'Promo2SinceYear': np.dtype(float)}
-                #'SchoolHoliday': np.dtype(str),
-                #'PromoInterval': np.dtype(str)}
+train, test = read_data()
+train = remove_outliers(train)
 
-    train = pd.read_csv(DATA_DIR+'train.csv', parse_dates=[2], dtype=types)
-    #test = pd.read_csv(DATA_DIR+'test.csv', parse_dates=[3], dtype=types)
-    store = pd.read_csv(DATA_DIR+'store.csv', dtype=types)
-    store_states = pd.read_csv(DATA_DIR+'store_states.csv', dtype=types)
-
-    print 'Merging Training and Test CSV with Store and State CSV'
-    train = pd.merge(train, store, on='Store')
-    train = pd.merge(train, store_states, on='Store')
-    #test = pd.merge(test, store, on='Store')
-    #test = pd.merge(test, store_states, on='Store')
-
-    columns = train.columns.tolist()
-    del(columns[columns.index('Sales')])
-    columns.append('Sales')
-    train = train[columns]
-
-    test = None
-    return train, test
-
-
-train, test = combine_data()
-data, features = build_features(train)
+#data, features = engineer_data(train)
 
